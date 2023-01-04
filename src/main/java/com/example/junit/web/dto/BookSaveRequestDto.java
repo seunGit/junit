c1ec99fb2cdf9fdcd2af6de3a39974b6e8f045f1
seungit <minsg3669@gmail.com>
2023-01-04 18:33:25 +0900

    책 등록_test 완료

---
+++ src/main/java/com/example/junit/web/dto/BookSaveRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.junit.web.dto;
+
+import com.example.junit.domain.Book;
+
+import lombok.Setter;
+
+@Setter // Controller에서 Setter가 호출되면서 Dto에 값이 채워짐.
+public class BookSaveRequestDto {
+    private String title;
+    private String author;
+
+    public Book toEntity() {
+        return Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+    }
+}
--- src/test/java/com/example/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junit/domain/BookRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
 
@@ -13,7 +16,21 @@ public class BookRepositoryTest {
     // 1. 책 등록
     @Test
     public void save_test(){
-        System.out.println("책등록_test 실행");
+        // given (데이터 준비)
+        String title = "junit5";
+        String author = "seunGit";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        // when ( 테스트 실행 )
+        Book bookPS = bookRepository.save(book);
+        // then (검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+
+        System.out.println("bookPS.getTitle = " + bookPS.getTitle());
+        System.out.println("bookPS.getAuthor = " + bookPS.getAuthor());
     }
     // 2. 책 목록보기
 
