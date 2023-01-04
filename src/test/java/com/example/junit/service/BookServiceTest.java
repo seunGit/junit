f7bb0863b455afed2ee0567d359c609eebce6fa9
seungit <minsg3669@gmail.com>
2023-01-05 02:58:00 +0900

    서비스 레이어 테스트 책등록하기 - stub, mock, assertJ 완료

--- src/test/java/com/example/junit/service/BookServiceTest.java
+++ src/test/java/com/example/junit/service/BookServiceTest.java
@@ -1,22 +1,38 @@
 package com.example.junit.service;
 
+import static org.assertj.core.api.Assertions.*;
+
 import com.example.junit.domain.BookRepository;
-import com.example.junit.util.MailSenderStub;
+import com.example.junit.util.MailSender;
 import com.example.junit.web.dto.BookResponseDto;
 import com.example.junit.web.dto.BookSaveRequestDto;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@DataJpaTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
-    @Autowired // DI
+//    @Autowired // DI
+//    private BookRepository bookRepository;
+//     문제점 -> 서비스만 테스트하고 싶은데, 레포지토리 레이어가 함께 테스트 된다는 점...!
+//               가짜 Repository, 가짜 MailSender...(익명 클래스) Mockito활용 : 가짜 객체 보관 환경
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Mock
     private BookRepository bookRepository;
-    // 문제점 -> 서비스만 테스트하고 싶은데, 레포지토리 레이어가 함께 테스트 된다는 점...!
-//               가짜 Repository, 가짜 MailSender... Mockito활용 : 가짜 객체 보관 환경
+
+    @Mock
+    private MailSender mailSender;
     @Test
     public void 책등록하기_테스트() {
         // given
@@ -24,16 +40,17 @@ public class BookServiceTest {
         dto.setTitle("junit강의");
         dto.setAuthor("seunGit");
 
-        // stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
-        // 가짜로 bookRepository 만들기 !!
+        // stub (가설)
+        when(bookRepository.save(any())).thenReturn(dto.toEntity());
+        when(mailSender.send()).thenReturn(true);
 
         // when
-        BookService bookService = new BookService(bookRepository, mailSenderStub);
         BookResponseDto bookResponseDto = bookService.책등록하기(dto);
 
         // then
-        assertEquals(dto.getTitle(), bookResponseDto.getTitle());
-        assertEquals(dto.getAuthor(), bookResponseDto.getAuthor());
+//        assertEquals(dto.getTitle(), bookResponseDto.getTitle());
+//        assertEquals(dto.getAuthor(), bookResponseDto.getAuthor());
+        assertThat(dto.getTitle()).isEqualTo(bookResponseDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookResponseDto.getAuthor());
     }
 }
