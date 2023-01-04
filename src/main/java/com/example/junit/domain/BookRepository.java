bada91fe1ba2b8bcbc8e3e8d6d013b7eaee603ab
seungit <minsg3669@gmail.com>
2023-01-04 13:45:12 +0900

    Repository test setting complete

--- src/main/java/com/example/junit/domain/BookRepository.java
+++ src/main/java/com/example/junit/domain/BookRepository.java
@@ -1,5 +1,7 @@
 package com.example.junit.domain;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
--- src/test/java/com/example/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junit/domain/BookRepositoryTest.java
@@ -1,4 +1,25 @@
 package com.example.junit.domain;
 
-public interface BookRepositoryTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
+public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void save_test(){
+        System.out.println("책등록_test 실행");
+    }
+    // 2. 책 목록보기
+
+    // 3. 책 한건 보기
+
+    // 4. 책 수정
+
+    // 5. 책 삭제
 }
